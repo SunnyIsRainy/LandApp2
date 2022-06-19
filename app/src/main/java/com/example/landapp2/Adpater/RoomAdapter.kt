a6074127bba709d73190af1ae4e69a1acc927e5c
Sun_Rain_git <mlkkmn9900@kakao.com>
2022-06-19 19:02:32 +0900

    Adapter 생성 및 상속, 오버라이딩을 통한 목록 UI를 설정

---
+++ app/src/main/java/com/example/landapp2/Adpater/RoomAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.landapp2.Adpater
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.example.landapp2.Data.Room
+import com.example.landapp2.R
+
+class RoomAdapter(
+    val mContext: Context,
+    val resId: Int,
+    val mList: ArrayList<Room>) : ArrayAdapter<Room>(mContext, resId, mList) {
+
+        val inflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+
+        if (tempRow == null) {
+            tempRow = inflater.inflate(R.layout.room_list_item, null)
+        }
+
+        val Row = tempRow!!
+
+    }
+
+}
