9197e596dd1abe7d7e9642fcf70f53fd2411b760
Sun_Rain_git <mlkkmn9900@kakao.com>
2022-06-19 20:07:42 +0900

    클릭 시 들어가는 화면 제작

--- app/src/main/java/com/example/landapp2/MainActivity.kt
+++ app/src/main/java/com/example/landapp2/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.landapp2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.landapp2.Adpater.RoomAdapter
@@ -22,5 +23,11 @@ class MainActivity : AppCompatActivity() {
         mRoomAdapter = RoomAdapter(this, R.layout.room_list_item, mRoomList)
         roomListView.adapter = mRoomAdapter
 
+        roomListView.setOnItemClickListener { parent, view, position, id ->
+            val roomData = mRoomList[position]
+            val myIntent = Intent(this, )
+        }
+
     }
+
 }
