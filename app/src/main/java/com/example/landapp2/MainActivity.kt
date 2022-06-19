31b9e547c752509d1bce5bc028e50dbc04ce35de
Sun_Rain_git <mlkkmn9900@kakao.com>
2022-06-19 19:13:47 +0900

    실제 데이터 입력 및 UI의 리스트에 Adapter을 사용해서 데이터 입력

--- app/src/main/java/com/example/landapp2/MainActivity.kt
+++ app/src/main/java/com/example/landapp2/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.landapp2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.landapp2.Adpater.RoomAdapter
+import com.example.landapp2.Data.Room
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val mRoomList = ArrayList<Room>()
+
+    lateinit var mRoomAdapter: RoomAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mRoomList.add( Room( 7500, "서울시 동대문구", 8, "서울시 동대문구의 8층 7500만원 방 입니다." ) )
+        mRoomList.add( Room( 12500, "서울시 동작구", 0, "서울시 동작구의 반지하 1억 2500만원 방 입니다." ) )
+
+        mRoomAdapter = RoomAdapter(this, R.layout.room_list_item, mRoomList)
+        roomListView.adapter = mRoomAdapter
+
     }
 }
